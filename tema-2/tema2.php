853e77ceaaae65cbdabcb75a4b4bf4f693bbc9ce
razvan Lodina <rlodina99@gmail.com>
2022-01-22 14:33:08 +0200

    Update tema2.php

--- tema-2/tema2.php
+++ tema-2/tema2.php
@@ -4,6 +4,15 @@
   echo date("d-m-Y");
 ?>
  </h1>
+
+ <h1>Adresa dvs IP este:</h1>
+ <pre>
+<?php
+  echo var_dump($_SERVER); //['REMOTE_ADDR'];
+  
+?>
+ </pre>
+
 <p>
     Acesta este un paragraf
 </p>
